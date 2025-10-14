4b18c67afc803d753c3e329401d1d5ba2d4ac52c
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-14 13:07:31 +0100

    Initial Commits

---
+++ app/cabins/page.js
@@ -0,0 +1,21 @@
+import Counter from "../components/Counter";
+
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  //   console.log(res);
+  const data = await res.json();
+  //   console.log(data);
+  return (
+    <ul>
+      {data.map((user) => (
+        <>
+          <li key={user.id}>
+            {user.name}: Lives at {user.address.suite}, {user.address.street}{" "}
+            {user.address.city}
+          </li>
+        </>
+      ))}
+      <Counter users={data} />
+    </ul>
+  );
+}
---
+++ app/components/Counter.js
@@ -0,0 +1,9 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Counter({ users }) {
+  console.log(users);
+  const [count, setCounter] = useState(0);
+  return <button onClick={() => setCounter((c) => c + 1)}>{count}</button>;
+}
