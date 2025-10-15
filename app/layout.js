325a8baa5f7334df08595d7971b53c7c63be2ad4
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-15 13:17:21 +0100

    Update Layout to include needed styles

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,16 @@ import Navigation from "./_components/Navigation";
 
 import "@/app/_styles/globals.css";
 
+import { Josefin_Sans } from "next/font/google";
+import Logo from "./_components/Logo";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+// console.log(josefin);
 export const metadata = {
   title: {
     template: "%s The Wild Oasis",
@@ -13,10 +23,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <Navigation />
-        <main className="mx-auto">{children}</main>
-        <footer>Copyright by the wild Oasis</footer>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased`}
+      >
+        <Header />
+        <div className="flex-1 bg-blue-800 px-8 py-12">
+          <main className="max-w-7xl px-10 mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
