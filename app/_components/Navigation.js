813352cbb8ca7ec830b8d47d366ab5302a2c4456
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-15 13:19:19 +0100

    Update Navigation

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,19 +1,34 @@
 import Link from "next/link";
-import Logo from "./Logo";
 
 export default function Navigation() {
   return (
-    <ul>
-      <Logo />
-      <li>
-        <Link href="/cabins">Cabins</Link>
-      </li>
-      <li>
-        <Link href="/account">Your Account</Link>
-      </li>
-      <li>
-        <Link href="/about">About</Link>
-      </li>
-    </ul>
+    <nav className="z-10 text-xl mt-4 md:mt-0">
+      <ul className="flex md:gap-16 gap-8 justify-center items-center md:justify-start">
+        <li>
+          <Link
+            href="/cabins"
+            className="hover:text-accent-400 transition-colors"
+          >
+            Cabins
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/about"
+            className="hover:text-accent-400 transition-colors"
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/account"
+            className="hover:text-accent-400 transition-colors"
+          >
+            Guest area
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
