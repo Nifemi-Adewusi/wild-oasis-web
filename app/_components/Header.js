c06d3b44521db1185bf8313ed012df3824b129be
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-16 13:45:37 +0100

    Update Tailwind styles for responsiveness

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -3,7 +3,7 @@ import Logo from "@/app/_components/Logo";
 
 function Header() {
   return (
-    <header className="border-b border-primary-900 px-8 py-5">
+    <header className="border-b border-primary-900 mt-24 md:mt-0 px-8 py-5">
       <div className="md:flex md:justify-between md:items-center max-w-7xl mx-auto">
         <Logo />
         <Navigation />
