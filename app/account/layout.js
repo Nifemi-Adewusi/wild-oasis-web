5bd263ce2d9da3d85aa636d09c929d695c412ce5
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-20 15:43:44 +0100

    Update the column styles

--- app/account/layout.js
+++ app/account/layout.js
@@ -1,6 +1,6 @@
 export default function Layout() {
   return (
-    <div className="grid grid-cols[16rem_1fr] h-full gap-12">
+    <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
       <div>Navigation</div>
       <div>Content</div>
     </div>
