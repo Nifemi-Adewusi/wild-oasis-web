84127a095797b660a722c4ea0723b35a82009d9e
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-22 15:35:30 +0100

    Update the error page

--- app/error.js
+++ app/error.js
@@ -1,10 +1,8 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 export default function Error({ error }) {
-  // const [buttonText, setButtonText] = useState("");
   const router = useRouter();
 
   const buttonText = window.history.length > 1 ? "Go Back" : "Go Home";
