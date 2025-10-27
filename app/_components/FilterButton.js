516fe9bea588faa5f16287b4423046ab5c3094b4
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-27 20:26:19 +0100

    Update and include the border-right

--- app/_components/FilterButton.js
+++ app/_components/FilterButton.js
@@ -1,10 +1,9 @@
 "use client";
 function Button({ children, handleFilterClick, active }) {
-  console.log("Active:", active);
   return (
     <button
-      className={`px-5 py-2 hover:bg-primary-700 border-r-2 ${
-        active ? "bg-primary-950 text-white" : ""
+      className={`px-5 py-2 hover:bg-primary-700 border-r-2 border-primary-200  ${
+        active ? "bg-primary-600" : ""
       }`}
       onClick={handleFilterClick}
     >
