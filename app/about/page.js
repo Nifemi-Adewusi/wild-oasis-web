eba134eea4e7adfcfd6c7cd5c2e5e5451147a582
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-27 06:40:31 +0100

    Dynamically fetched the number of cabins data from the API

--- app/about/page.js
+++ app/about/page.js
@@ -2,13 +2,17 @@ import Image from "next/image";
 import image1 from "@/public/about-1.jpg";
 import image2 from "@/public/about-2.jpg";
 import Wrapper from "../_components/Wrapper";
+import { getCabins } from "../_lib/data-service";
 
 /* eslint-disable react/no-unescaped-entities */
 export const metadata = {
   title: "About Us",
 };
 
-export default function Page() {
+export const revalidate = 86400;
+
+export default async function Page() {
+  const cabins = await getCabins();
   return (
     <Wrapper>
       <div className="grid grid-cols-1 md:grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
@@ -26,10 +30,11 @@ export default function Page() {
               enjoying simple pleasures with family.
             </p>
             <p>
-              Our 8 luxury cabins provide a cozy base, but the real freedom and
-              peace you'll find in the surrounding mountains. Wander through
-              lush forests, breathe in the fresh air, and watch the stars
-              twinkle above from the warmth of a campfire or your hot tub.
+              Our {cabins?.length} luxury cabins provide a cozy base, but the
+              real freedom and peace you'll find in the surrounding mountains.
+              Wander through lush forests, breathe in the fresh air, and watch
+              the stars twinkle above from the warmth of a campfire or your hot
+              tub.
             </p>
             <p>
               This is where memorable moments are made, surrounded by nature's
