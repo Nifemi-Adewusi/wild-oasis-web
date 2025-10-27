7e4aa3384b3a7d21683aebbfe5d82ec196311059
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-27 20:16:10 +0100

    Used the Filter Button Component instance and passed down the needed props

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -1,6 +1,22 @@
 "use client";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import Button from "./FilterButton";
+
+const filterButtonDetails = [
+  {
+    label: "All Cabins",
+    filteredBy: "all",
+  },
+  {
+    label: "1 - 4 guests",
+    filteredBy: "small",
+  },
+  {
+    label: "5 - 8 guests",
+    filteredBy: "medium",
+  },
+];
 
 export default function Filter({ queryKey }) {
   // Returns the query String e.g ?capacity = small
@@ -8,6 +24,8 @@ export default function Filter({ queryKey }) {
   const router = useRouter();
   //   Returns the path or the current route we're on e.g /cabins
   const pathName = usePathname();
+
+  const activeFilter = searchParams.get(queryKey) || "all";
   const handleFilterClick = (filter) => {
     const params = new URLSearchParams(searchParams);
     params.set(queryKey, filter);
@@ -15,13 +33,23 @@ export default function Filter({ queryKey }) {
   };
   return (
     <div className="border border-primary-800 flex ">
-      <button
+      {filterButtonDetails.map((filterButton) => (
+        <Button
+          key={filterButton.label}
+          active={activeFilter === filterButton.filteredBy}
+          handleFilterClick={() => handleFilterClick(filterButton.filteredBy)}
+        >
+          {filterButton.label}
+        </Button>
+      ))}
+      {/* <button
         className="px-5 py-2 hover:bg-primary-700 border-r-2"
         onClick={() => handleFilterClick("all")}
       >
         All Cabins
-      </button>
-      <button
+      </button> */}
+      {/* <Button>All Cabins</Button> */}
+      {/* <button
         className="px-5 py-2 hover:bg-primary-700 border-r-2"
         onClick={() => handleFilterClick("small")}
       >
@@ -32,7 +60,19 @@ export default function Filter({ queryKey }) {
         onClick={() => handleFilterClick("medium")}
       >
         5 &mdash; 8 guests
-      </button>
+      </button> */}
     </div>
   );
 }
+
+// function Button({ children, filteredBy, handleFilterClick }) {
+//   return (
+//     <button
+//       className="px-5 py-2 hover:bg-primary-700"
+//       onClick={() => handleFilterClick(filteredBy)}
+//     >
+//       {/* 5 &mdash; 8 guests */}
+//       {children}
+//     </button>
+//   );
+// }
