bfd0fb83454dc93a7128420012ab2d1956820327
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-27 06:43:37 +0100

    used the filters to display the cabins

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -4,7 +4,7 @@ import CabinCard from "./CabinCard";
 import Spinner from "./Spinner";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
-export default function CabinList() {
+export default function CabinList({ filter }) {
   const {
     data: cabins,
     error,
@@ -14,10 +14,21 @@ export default function CabinList() {
   });
   if (isLoading) return <Spinner />;
   if (error) throw new Error("Failed to load cabins");
-  if (!cabins?.length) return <p>No cabins found</p>;
+  let filteredCabins = cabins;
+  if (filter === "small") {
+    filteredCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  }
+  if (filter === "medium") {
+    filteredCabins = cabins.filter(
+      (cabin) => cabin.maxCapacity > 3 && cabin.maxCapacity <= 7
+    );
+  }
+  if (filter === "large") {
+    filteredCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+  }
   return (
     <div className="grid md:grid-cols-2  gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {filteredCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
