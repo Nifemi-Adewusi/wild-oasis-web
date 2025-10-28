938107679552e36682c8f0d383717a4079deab2a
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-28 15:16:14 +0100

    Update FilterButton

--- app/_components/FilterButton.js
+++ app/_components/FilterButton.js
@@ -3,7 +3,7 @@ function Button({ children, handleFilterClick, active }) {
   return (
     <button
       className={`px-5 py-2 hover:bg-primary-700 border-r-2 border-primary-200  ${
-        active ? "bg-primary-600" : ""
+        active ? "bg-primary-600 text-primary-300" : ""
       }`}
       onClick={handleFilterClick}
     >
