09cf8f03410f639f5a96e77f771673c015c26614
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-10-30 13:17:15 +0100

    Create the reservation component that imports the needed services from the backend

---
+++ app/_components/Reservation.js
@@ -0,0 +1,16 @@
+import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
+import DateSelector from "./DateSelector";
+import ReservationForm from "./ReservationForm";
+
+export default async function Reservation({ cabin }) {
+  const [settings, bookingDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+  return (
+    <div className="grid md:grid-cols-2  border border-primary-800 min-h-[400px]">
+      <DateSelector />
+      <ReservationForm />
+    </div>
+  );
+}
