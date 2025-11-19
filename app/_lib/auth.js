84c520a1ff9a0afcf3c5444c055571efc5844e1f
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-11-19 10:49:14 +0100

    Update the auth file

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,16 +1,20 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-
-const authConfig = {
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
-};
+import GoogleProvider from "next-auth/providers/google";
 
 export const {
-  auth,
   handlers: { GET, POST },
-} = NextAuth(authConfig);
+  auth,
+} = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+  },
+});
