43a0ad1400d44f843c797df550cbfaac2d541960
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-11-19 13:13:30 +0100

    Update the file

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -27,9 +27,9 @@ function ReservationForm({ cabin, bookingDates, user }) {
           </div>
         </div>
       </div>
-      <p>
+      {/* <p>
         {String(range?.from)} to {String(range?.to)}
-      </p>
+      </p> */}
       <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
         <div className="space-y-2">
           <label htmlFor="numGuests">How many guests?</label>
