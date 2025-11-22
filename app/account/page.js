c31e5ca2b7ba4857f855c6c3efafb66586000225
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-11-22 18:13:31 +0100

    Update and dynamically got the user session on the account page

--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,15 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Your User Account",
 };
 
-export default function Page() {
-  return <h1>Account Page</h1>;
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+  return (
+    <h1 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome {firstName}
+    </h1>
+  );
 }
